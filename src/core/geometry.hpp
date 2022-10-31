6e773efcc116c0c6888ead28a9e984535f51234f
eka <phyzheye01@gmail.com>
2022-10-31 13:08:20 -0600

    feat(core/geomtry): added more methods to vector3 class template

--- src/core/geometry.hpp
+++ src/core/geometry.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include <algorithm>
+#include <cmath>
+#include <stdlib.h>
 
 template <typename T> class Vector2 {
 public:
@@ -127,10 +130,67 @@ public:
   }
 
   Vector3<T> operator-() const { return Vector3<T>(-x, -y, -z); }
+
+  float LengthSquared() const { return x * x + y * y + z * z; }
+  float Length() const { return std::sqrt(LengthSquared()); }
 };
 
 template <typename T> inline Vector3<T> operator*(T s, const Vector3<T> &v) {
   return v * s;
+}
+
+template <typename T> inline T Dot(const Vector3<T> &v1, const Vector3<T> &v2) {
+  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+template <typename T>
+inline T AbsDot(const Vector3<T> &v1, const Vector3<T> &v2) {
+  return std::abs(Dot(v1, v2));
+}
+
+template <typename T>
+inline Vector3<T> Cross(const Vector3<T> &v1, const Vector3<T> &v2) {
+  double v1x = v1.x, v1y = v1.y, v1z = v1.z;
+  double v2x = v2.x, v2y = v2.y, v2z = v2.z;
+  return Vector3<T>((v1y * v2z) - (v1z * v2y), (v1z * v2x) - (v1x * v2z),
+                    (v1x * v2y) - (v1y * v2x));
+}
+
+template <typename T> inline Vector3<T> Normalize(const Vector3<T> &v) {
+  return v / v.Length();
+}
+
+template <typename T> T MinComponent(const Vector3<T> &v) {
+  return std::min(v.x, std::min(v.y, v.z));
+}
+template <typename T> T MaxComponent(const Vector3<T> &v) {
+  return std::max(v.x, std::max(v.y, v.z));
+}
+
+template <typename T> int MaxDimension(const Vector3<T> &v) {
+  return (v.x > v.y) ? ((v.x > v.z) ? 0 : 2) : ((v.y > v.z) ? 1 : 2);
+}
+
+template <typename T>
+Vector3<T> Min(const Vector3<T> &p1, const Vector3<T> &p2) {
+  return Vector3<T>(std::min(p1.x, p2.x), std::min(p1.y, p2.y),
+                    std::min(p1.z, p2.z));
+}
+
+template <typename T>
+Vector3<T> Max(const Vector3<T> &p1, const Vector3<T> &p2) {
+  return Vector3<T>(std::max(p1.x, p2.x), std::max(p1.y, p2.y),
+                    std::max(p1.z, p2.z));
+}
+
+template <typename T>
+inline void CoordinateSystem(const Vector3<T> &v1, Vector3<T> *v2,
+                             Vector3<T> *v3) {
+  if (std::abs(v1.x) > std::abs(v1.y))
+    *v2 = Vector3<T>(-v1.z, 0, v1.x) / std::sqrt(v1.x * v1.x + v1.z * v1.z);
+  else
+    *v2 = Vector3<T>(0, v1.z, -v1.y) / std::sqrt(v1.y * v1.y + v1.z * v1.z);
+  *v3 = Cross(v1, *v2);
 }
 
 typedef Vector2<float> Vector2f;
