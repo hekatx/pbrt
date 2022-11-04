314f4fab7ea928b994fa8761b8da4efde50690be
eka <phyzheye01@gmail.com>
2022-11-04 16:06:44 -0600

    feat(core/transform): added transform class

--- src/core/transform.hpp
+++ src/core/transform.hpp
@@ -1,4 +1,9 @@
+#include "geometry.hpp"
 #include <ostream>
+
+static float Pi = 3.14159265358979323846;
+
+inline float Radians(float deg) { return (Pi / 180) * deg; }
 
 struct Matrix4x4 {
   // Matrix4x4 Public Methods
@@ -51,3 +56,148 @@ struct Matrix4x4 {
 
   float m[4][4];
 };
+
+class Transform {
+public:
+  Transform(){};
+  Transform(const Matrix4x4 &m) : m(m), mInv(Inverse(m)) {}
+  Transform(const float mat[4][4]) {
+    m = Matrix4x4(mat[0][0], mat[0][1], mat[0][2], mat[0][3], mat[1][0],
+                  mat[1][1], mat[1][2], mat[1][3], mat[2][0], mat[2][1],
+                  mat[2][2], mat[2][3], mat[3][0], mat[3][1], mat[3][2],
+                  mat[3][3]);
+    mInv = Inverse(m);
+  }
+  Transform(const Matrix4x4 &m, const Matrix4x4 &mInv) : m(m), mInv(mInv) {}
+
+  friend Transform Inverse(const Transform &t) {
+    return Transform(t.mInv, t.m);
+  }
+
+  friend Transform Transpose(const Transform &t) {
+    return Transform(Transpose(t.m), Transpose(t.mInv));
+  }
+  Transform Translate(const Vector3f &delta) {
+    Matrix4x4 m(1, 0, 0, delta.x, 0, 1, 0, delta.y, 0, 0, 1, delta.z, 0, 0, 0,
+                1);
+    Matrix4x4 minv(1, 0, 0, -delta.x, 0, 1, 0, -delta.y, 0, 0, 1, -delta.z, 0,
+                   0, 0, 1);
+    return Transform(m, minv);
+  }
+
+  Transform Scale(float x, float y, float z) {
+    Matrix4x4 m(x, 0, 0, 0, 0, y, 0, 0, 0, 0, z, 0, 0, 0, 0, 1);
+    Matrix4x4 minv(1 / x, 0, 0, 0, 0, 1 / y, 0, 0, 0, 0, 1 / z, 0, 0, 0, 0, 1);
+    return Transform(m, minv);
+  }
+
+  bool HasScale() const {
+    float la2 = (*this)(Vector3f(1, 0, 0)).LengthSquared();
+    float lb2 = (*this)(Vector3f(0, 1, 0)).LengthSquared();
+    float lc2 = (*this)(Vector3f(0, 0, 1)).LengthSquared();
+#define NOT_ONE(x) ((x) < .999f || (x) > 1.001f)
+    return (NOT_ONE(la2) || NOT_ONE(lb2) || NOT_ONE(lc2));
+#undef NOT_ONE
+  }
+  template <typename T> inline Point3<T> operator()(const Point3<T> &p) const;
+  template <typename T> inline Vector3<T> operator()(const Vector3<T> &v) const;
+
+  Transform RotateX(float theta) {
+    float sinTheta = std::sin(Radians(theta));
+    float cosTheta = std::cos(Radians(theta));
+    Matrix4x4 m(1, 0, 0, 0, 0, cosTheta, -sinTheta, 0, 0, sinTheta, cosTheta, 0,
+                0, 0, 0, 1);
+    return Transform(m, Transpose(m));
+  }
+
+  Transform RotateZ(float theta) {
+    float sinTheta = std::sin(Radians(theta));
+    float cosTheta = std::cos(Radians(theta));
+    Matrix4x4 m(cosTheta, -sinTheta, 0, 0, sinTheta, cosTheta, 0, 0, 0, 0, 1, 0,
+                0, 0, 0, 1);
+    return Transform(m, Transpose(m));
+  }
+
+  Transform RotateY(float theta) {
+    float sinTheta = std::sin(Radians(theta));
+    float cosTheta = std::cos(Radians(theta));
+    Matrix4x4 m(cosTheta, 0, sinTheta, 0, 0, 1, 0, 0, -sinTheta, 0, cosTheta, 0,
+                0, 0, 0, 1);
+    return Transform(m, Transpose(m));
+  }
+
+  Transform Rotate(float theta, const Vector3f &axis) {
+    Vector3f a = Normalize(axis);
+    float sinTheta = std::sin(Radians(theta));
+    float cosTheta = std::cos(Radians(theta));
+    Matrix4x4 m;
+    m.m[0][0] = a.x * a.x + (1 - a.x * a.x) * cosTheta;
+    m.m[0][1] = a.x * a.y * (1 - cosTheta) - a.z * sinTheta;
+    m.m[0][2] = a.x * a.z * (1 - cosTheta) + a.y * sinTheta;
+    m.m[0][3] = 0;
+
+    m.m[1][0] = a.x * a.y * (1 - cosTheta) + a.z * sinTheta;
+    m.m[1][1] = a.y * a.y + (1 - a.y * a.y) * cosTheta;
+    m.m[1][2] = a.y * a.z * (1 - cosTheta) - a.x * sinTheta;
+    m.m[1][3] = 0;
+
+    m.m[2][0] = a.x * a.z * (1 - cosTheta) - a.y * sinTheta;
+    m.m[2][1] = a.y * a.z * (1 - cosTheta) + a.x * sinTheta;
+    m.m[2][2] = a.z * a.z + (1 - a.z * a.z) * cosTheta;
+    m.m[2][3] = 0;
+
+    return Transform(m, Transpose(m));
+  }
+
+  Transform LookAt(const Point3f &pos, const Point3f &look,
+                   const Vector3f &up) {
+    Matrix4x4 cameraToWorld;
+    cameraToWorld.m[0][3] = pos.x;
+    cameraToWorld.m[1][3] = pos.y;
+    cameraToWorld.m[2][3] = pos.z;
+    cameraToWorld.m[3][3] = 1;
+
+    Vector3f dir = Normalize(look - pos);
+    Vector3f right = Normalize(Cross(Normalize(up), dir));
+    Vector3f newUp = Cross(dir, right);
+    cameraToWorld.m[0][0] = right.x;
+    cameraToWorld.m[1][0] = right.y;
+    cameraToWorld.m[2][0] = right.z;
+    cameraToWorld.m[3][0] = 0.;
+    cameraToWorld.m[0][1] = newUp.x;
+    cameraToWorld.m[1][1] = newUp.y;
+    cameraToWorld.m[2][1] = newUp.z;
+    cameraToWorld.m[3][1] = 0.;
+    cameraToWorld.m[0][2] = dir.x;
+    cameraToWorld.m[1][2] = dir.y;
+    cameraToWorld.m[2][2] = dir.z;
+    cameraToWorld.m[3][2] = 0.;
+
+    return Transform(Inverse(cameraToWorld), cameraToWorld);
+  }
+
+private:
+  Matrix4x4 m, mInv;
+};
+
+template <typename T>
+inline Point3<T> Transform::operator()(const Point3<T> &p) const {
+  T x = p.x, y = p.y, z = p.z;
+  T xp = m.m[0][0] * x + m.m[0][1] * y + m.m[0][2] * z + m.m[0][3];
+  T yp = m.m[1][0] * x + m.m[1][1] * y + m.m[1][2] * z + m.m[1][3];
+  T zp = m.m[2][0] * x + m.m[2][1] * y + m.m[2][2] * z + m.m[2][3];
+  T wp = m.m[3][0] * x + m.m[3][1] * y + m.m[3][2] * z + m.m[3][3];
+  CHECK_NE(wp, 0);
+  if (wp == 1)
+    return Point3<T>(xp, yp, zp);
+  else
+    return Point3<T>(xp, yp, zp) / wp;
+}
+
+template <typename T>
+inline Vector3<T> Transform::operator()(const Vector3<T> &v) const {
+  T x = v.x, y = v.y, z = v.z;
+  return Vector3<T>(m.m[0][0] * x + m.m[0][1] * y + m.m[0][2] * z,
+                    m.m[1][0] * x + m.m[1][1] * y + m.m[1][2] * z,
+                    m.m[2][0] * x + m.m[2][1] * y + m.m[2][2] * z);
+}
